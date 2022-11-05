0b557d3dc071a80b7f5fa384ae7998a588c17ed7
zanspiler <zanspiler1@gmail.com>
2022-11-05 17:51:53 +0100

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-plusplus': 'off',
+    'vuejs-accessibility/form-control-has-label': 'off',
+    'prefer-destructuring': 'off',
+    'no-multi-str': 'off',
+    'vue/html-quotes': ['error', 'double', { avoidEscape: false }],
+    'vuejs-accessibility/click-events-have-key-events': 'off',
+    'vue/no-multiple-template-root': 'off',
+  },
+};
