386edceb5aef1d9fd1eace92652e53a3c2e54b0f
zanspiler <zanspiler1@gmail.com>
2022-12-26 20:05:44 +0100

    convert preferences store to TypeScript

--- src/stores/preferences.ts
+++ src/stores/preferences.ts
@@ -1,10 +1,8 @@
 import { defineStore } from 'pinia'
 import { useStorage } from '@vueuse/core'
 
-const modes = {
-  buses: 'BUSES',
-  stations: 'STATIONS',
-}
+// eslint-disable-next-line no-unused-vars, no-shadow
+enum Mode { buses, stations }
 
 function userPrefersDarkTheme() {
   return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
@@ -13,14 +11,14 @@ function userPrefersDarkTheme() {
 export const usePreferencesStore = defineStore('preferences', {
   state: () => ({
     darkTheme: useStorage('darkTheme', userPrefersDarkTheme()),
-    mode: useStorage('mode', modes.stations),
+    mode: useStorage('mode', Mode.stations),
   }),
   actions: {
     toggleMode() {
-      this.mode = this.mode === modes.buses ? modes.stations : modes.buses
+      this.mode = this.mode === Mode.buses ? Mode.stations : Mode.buses
     },
   },
   getters: {
-    isInStationsMode: (state) => state.mode === modes.stations,
+    isInStationsMode: (state) => state.mode === Mode.stations,
   },
 })
